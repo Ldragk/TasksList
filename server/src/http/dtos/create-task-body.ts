685185c60f6b9f3b0e672091954c256bd72611f2
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-02-13 23:53:13 -0300

    ⚒  4º sprint - Validation of the task entity

--- server/src/http/dtos/create-task-body.ts
+++ server/src/http/dtos/create-task-body.ts
@@ -1,12 +1,29 @@
-export class TaskBody {  
+import { IsNotEmpty, IsUUID, Length, ValidateIf } from "class-validator";
+import { numberOfDaysInTheMonth } from "../../use-cases/notifications-cases/functions/numberOfDaysInTheMonth";
+
+export class TaskBody {
+  @IsNotEmpty()
+  @Length(1, 30)
   title!: string;
+
+  @Length(5, 500)
   description!: string;
+
+  @ValidateIf((o) => o.limitDay >= 1 && o.limitDay <= numberOfDaysInTheMonth())
   limitDay!: number;
+
+  @ValidateIf((o) => o.limitMonth >= 1 && o.limitMonth <= 12)
   limitMonth!: number;
-  limitYear!: number; 
-  date?: string | undefined | null
+
+  @ValidateIf((o) => o.limitYear >= 2023)
+  limitYear!: number;
+
+  date?: string | undefined | null;
   done?: boolean;
   createdAt?: Date;
-  updatedAt?: Date;    
+  updatedAt?: Date;
+
+  @IsNotEmpty()
+  @IsUUID()
   id?: string;
 }
