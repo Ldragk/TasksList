a02dfff7dfe8b6136f803f022aebcf7d4af24d03
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-06-05 00:57:56 -0300

    ✬ Fixing wrong changed code

--- server/src/repositories/in-memory-repository/in-memory-query-repository.ts
+++ server/src/repositories/in-memory-repository/in-memory-query-repository.ts
@@ -4,8 +4,7 @@ import { InMemoryManageRepository } from "./in-memory-manage-repository";
 
 export class InMemoryQueryRepository
   extends InMemoryManageRepository
-  implements QueryRepository
-{
+  implements QueryRepository {
   async findAllTasks(): Promise<Task[]> {
     return this.tasks;
   }
@@ -16,35 +15,33 @@ export class InMemoryQueryRepository
 
   async findByMonth(month: number, days: number[], year: number): Promise<Task[]> {
     const tasksMonth: Task[] = [];
-  
-    const targetDate = `${month}/${days[0]}/${year}`;
-  
-    for (const task of this.tasks) {
-      if (task.date.value === targetDate) {
-        tasksMonth.push(task);
-      }
+    for (const d of days) {
+      this.tasks.forEach((task: Task) => {
+        if (task.date.value === `${month}/${d}/${year}`) {
+          tasksMonth.push(task);
+        }
+      });
     }
-  
     return tasksMonth;
   }
 
   async findByYear(month: number[], days: number[], year: number): Promise<Task[]> {
     const tasksYear: Task[] = [];
-  
+
     for (const task of this.tasks) {
       const [taskMonth, taskDay, taskYear] = task.date.value.split('/');
       const taskMonthNumber = parseInt(taskMonth, 10);
       const taskDayNumber = parseInt(taskDay, 10);
       const taskYearNumber = parseInt(taskYear, 10);
-  
-      if (month.includes(taskMonthNumber) 
-      && days.includes(taskDayNumber) 
-      && taskYearNumber === year) {
+
+      if (month.includes(taskMonthNumber)
+        && days.includes(taskDayNumber)
+        && taskYearNumber === year) {
         tasksYear.push(task);
       }
-    }  
+    }
     return tasksYear;
-  }  
+  }
 
   async findByStatus(condition: boolean): Promise<Task[]> {
     return this.tasks.filter((task) => task.done === condition);
